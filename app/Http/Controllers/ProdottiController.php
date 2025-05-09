ae15d9fd7dedcfc8cfe2293c711527f14043fbd2
Ema998 <emanuele.gornati3@gmail.com>
2025-05-09 07:55:26 +0200

    commit

---
+++ app/Http/Controllers/ProdottiController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Game;
+use App\Http\Requests\ProdottiRequest;
+
+class ProdottiController extends Controller
+{
+    public function store(ProdottiRequest $request)
+    {
+        $nome = $request->input('nome');
+        $prezzo = $request->input('prezzo');
+        $descrizione = $request->input('descrizione');
+        $img = null;
+
+        if($request->file('img')){
+            $img = $request->file('img')->store('public/img');
+        }
+        
+        
+        $prodotto = Game::create([
+            'nome' => $nome,
+            'prezzo' => $prezzo,
+            'descrizione' => $descrizione,
+            'img' => $img
+        ]);
+
+        return redirect()->route('prodotti')->with('message', 'Prodotto aggiunto con successo!');
+    }
+
+    public function prodottiList(){
+        $prodotti = Game::all();
+        return view('prodotto.prodotti', ['prodotti' => $prodotti]);
+    }
+}
