42755508e460abf147022afad8c5ef646d53e0cb
Ema998 <emanuele.gornati3@gmail.com>
2025-07-02 19:13:53 +0200

    commit

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,5 @@ use App\Http\Controllers\ProdottiController;
 
 Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
 Route::get('/prodotti', [PublicController::class, 'prodotti'])->name('prodotti');
-Route::post('/aggiungiProdotto', [PublicController::class, 'aggiungiProdotto'])->name('aggiungiProdotto');
+Route::get('/aggiungiProdotto', [PublicController::class, 'aggiungiProdotto'])->name('aggiungiProdotto');
 Route::post('/aggiungiProdottoSubmit', [ProdottiController::class, 'store'])->name('aggiungiProdottoSubmit');
